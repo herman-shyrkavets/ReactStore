8d12c2ad950bb63190ef6afb5fad5f6ed631ebd8
herman-shyrkavets <gsiruovec@gmail.com>
2026-07-08 00:09:42 +0300

    feat: set up React Router and create basic pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import './App.css';
+import { BrowserRouter, Route, Routes, Link} from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+      <nav className="navbar">
+        <Link to="/">Главная</Link>
+        <Link to="/products">Каталог</Link>
+        <Link to="/about">О нас</Link>
+      </nav>
+
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/products" element={<Products />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Products.jsx
@@ -0,0 +1,8 @@
+export default function Products() {
+    return (
+        <div>
+            <h1>Каталог товаров</h1>
+            <p>Здесь скоро будут карточки товаров.</p>
+        </div>
+    );
+}
