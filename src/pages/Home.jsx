dc1c5bddf814684aae172c8215970901f05d614b
herman-shyrkavets <gsiruovec@gmail.com>
2026-07-15 20:58:41 +0300

    feat: A reusable UI component Button has been created and navigation has been fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
+import Button from '../ui/Button';
+import { useNavigate } from 'react-router-dom';
+
 export default function Home() {
+    const navigate = useNavigate();
+
     return (
         <div>
             <h1>Главная страница</h1>
             <p>Добро пожаловать в React Store</p>
+
+            <Button onClick={() => navigate("/products")}>
+                Перейти в каталог
+            </Button>
         </div>
     );
 }
